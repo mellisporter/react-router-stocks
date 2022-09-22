c0ef3afbc7f75ef61db0023ea41f575f7c7546f5
Matt Porter <matthew.ellis.porter@gmail.com>
2022-09-22 18:00:32 -0400

    Nav Bar component added

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Nav = function() {
+    return(
+        <div className="nav"> 
+<a>About</a>
+<a>Stocks</a>
+        </div>
+    )
+}
+
+export default Nav;
