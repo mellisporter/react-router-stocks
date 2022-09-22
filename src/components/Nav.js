85241577d09a2475f4470fd9b2cbb219659bdee9
Matt Porter <matthew.ellis.porter@gmail.com>
2022-09-22 18:05:09 -0400

    nav bar styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,9 @@ import {Link} from "react-router-dom";
 const Nav = function() {
     return(
         <div className="nav"> 
-<a>About</a>
-<a>Stocks</a>
+        StockR
+        <a>About</a>
+        <a>Stocks</a>
         </div>
     )
 }
